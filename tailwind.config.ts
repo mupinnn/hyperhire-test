9ca0571e610606ca6816233cef4fcaa1e054d453
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-12-05 19:48:23 +0700

    chore: setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      'trabuddy-alternative': '#00C696',
+      'nephritis/05': '#E9F7EF',
+      'light-sky-blue': '#8BC4FF',
+      'dark-sky-blue': '#82BFD4',
+      'yellow-sun': '#FBFF23',
+      'blue-gray/80': '#343741',
+      'blue-gray/60': '#5E626F',
+      'blue-gray/40': '#C1C5CF',
+      'anti-flash-white': '#EFF1F6',
+      'shadow-blue': '#7A8FAD',
+      blueberry: '#4A77FF',
+      turquoise: '#40E2E8',
+      azure: '#EDFCFF',
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
